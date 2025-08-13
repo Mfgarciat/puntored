738584765413e7a1b84cc43b9f520fdeee594c13
Mfgarciat <mfgarciat@ufpso.edu.co>
2025-08-13 09:49:24 -0500

    update application.properties

--- src/test/java/com/test/puntored/PuntoredApplicationTests.java
+++ src/test/java/com/test/puntored/PuntoredApplicationTests.java
@@ -2,9 +2,10 @@ package com.test.puntored;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-
+@ActiveProfiles("test")
 class PuntoredApplicationTests {
 
 	@Test
